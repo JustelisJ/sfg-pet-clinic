ea498d0a0c657e99260e072867137e8d13bf1233
JustelisJ <j.jancys@gmail.com>
2020-10-19 15:33:01 +0300

    Added spring data jpa for owner. Changed annotation in Pet.java class Closes #39

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -8,12 +8,12 @@ import java.util.Set;
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
-    
+
     @ManyToOne
-    @Column(name = "type_id")
+    @JoinColumn(name = "type_id")
     private PetType petType;
     @ManyToOne
-    @Column(name = "owner_id")
+    @JoinColumn(name = "owner_id")
     private Owner owner;
     @Column(name = "birth_day")
     private LocalDate birthday;
